cef988d756a91258e811f67ccfd01214faec36ac
Rafael <faelbarbosa1790@gmail.com>
2026-02-05 12:28:49 -0300

    Create Identificador de idade.py

---
+++ Identificador de idade.py
@@ -0,0 +1,6 @@
+#Indetificador de idade
+idade = int(input("Qual a sua idade?: "))
+if idade <18:
+    print("Entrada não permitida! Somente maiores de 18 anos")
+else:
+    print("Entrada permitida. Bem-Vindo!")
